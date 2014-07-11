ddf95aac53db2501e038467c64c501604f559b48
Aojia Zhao <aojia93@stanford.edu>
2014-07-11 16:59:32 -0500

    Initiate Project

---
+++ iBay/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IBay::Application.config.secret_key_base = '8c1cbbda1e82375d4ce45507b265da1af121f77eb090c4117ef0f6fed322886002e72054ada9b104eb875dc4dd0fabd527c3bee6779fb37fa5de722da4f28e5b'
